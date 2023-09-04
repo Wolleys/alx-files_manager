528bda301e73c84ad69c951b887bdd402e899dbf
Wolleys <wolleysmigaya@gmail.com>
2023-09-04 06:24:11 +0000

    Update MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,8 @@
 import mongodb from 'mongodb';
+// eslint-disable-next-line no-unused-vars
+import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
+
 /**
  * Represents a MongoDB client.
  */
@@ -7,13 +11,16 @@ class DBClient {
    * Creates a new DBClient instance.
    */
   constructor() {
+    envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
     const dbURL = `mongodb://${host}:${port}/${database}`;
+
     this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
     this.client.connect();
   }
+
   /**
    * Checks if this client's connection to the MongoDB server is active.
    * @returns {boolean}
@@ -21,6 +28,7 @@ class DBClient {
   isAlive() {
     return this.client.isConnected();
   }
+
   /**
    * Retrieves the number of users in the database.
    * @returns {Promise<Number>}
@@ -28,6 +36,7 @@ class DBClient {
   async nbUsers() {
     return this.client.db().collection('users').countDocuments();
   }
+
   /**
    * Retrieves the number of files in the database.
    * @returns {Promise<Number>}
@@ -38,7 +47,7 @@ class DBClient {
 
   /**
    * Retrieves a reference to the `users` collection.
-   * @returns {Promise<mongodb.Collection>}
+   * @returns {Promise<Collection>}
    */
   async usersCollection() {
     return this.client.db().collection('users');
@@ -46,7 +55,7 @@ class DBClient {
 
   /**
    * Retrieves a reference to the `files` collection.
-   * @returns {Promise<mongodb.Collection>}
+   * @returns {Promise<Collection>}
    */
   async filesCollection() {
     return this.client.db().collection('files');
